b650ee21ad9e16b17636e43dd850f011c8cf7ee1
anatol <anatolminor91@inbox.ru>
2021-10-17 15:19:13 +0300

    class

--- launcher.py
+++ launcher.py
@@ -1,17 +1,33 @@
 import subprocess
+import os
+import time
+
+
+d = os.getcwd()
 
 process = []
 
 while True:
-    action = input('Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
+    action = input(
+        'Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
 
     if action == 'q':
         break
     elif action == 's':
-        process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
-        process.append(subprocess.Popen('python client.py -n test1', creationflags=subprocess.CREATE_NEW_CONSOLE))
-        process.append(subprocess.Popen('python client.py -n test2', creationflags=subprocess.CREATE_NEW_CONSOLE))
-        process.append(subprocess.Popen('python client.py -n test3', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        process.append(subprocess.Popen(
+            'gnome-terminal -- bash -c "python3 server.py"', shell=True))
+        time.sleep(1)
+        process.append(subprocess.Popen(
+            'gnome-terminal -- bash -c "python3 client.py -n test1"', shell=True))
+        time.sleep(1)
+        process.append(subprocess.Popen(
+            'gnome-terminal -- bash -c "python3 client.py -n test2"', shell=True))
+        time.sleep(1)
+        process.append(subprocess.Popen(
+            'gnome-terminal -- bash -c "python3 client.py -n test3"', shell=True))
+        # subprocess.call(['gnome-terminal', '--', 'bash',
+        #                 '-c' 'python3 server.py'])
+        print(process)
     elif action == 'x':
         while process:
             victim = process.pop()
